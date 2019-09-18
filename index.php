3e12e3cac266740ad9624b93090964392318eb54
MyRuonis <kciulcinskas@gmail.com>
2019-09-18 16:20:07 +0300

    (Empty) 3 new pages

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 
 <h2>Welcome !!!</h2>
 
+<button type="button" href="administravimas.php">Eilės administravimas</button> 
+<button type="button" href="svieslente.php">Švieslentė</button> 
+<button type="button" href="specialistas.php">Specialistas</button> 
+
 </body>
