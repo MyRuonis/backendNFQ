c81cd4c143734cce918b00b17870b1ee49b4c1e0
MyRuonis <kciulcinskas@gmail.com>
2019-09-18 18:12:58 +0300

    bug fix

--- sukurtiKlienta.php
+++ sukurtiKlienta.php
@@ -7,7 +7,11 @@ include 'connect.php';
 
 $query = "INSERT INTO patients VALUES($vard, $time)";
 
+echo "HERE1";
+
 pg_prepare(, "prepare1", $query) or die ("Cannot prepare statement\n"); 
+
+echo "HERE2";
 
 pg_execute($conn, "prepare1", array($vard, $time)) or die ("Cannot execute statement\n"); 
 
