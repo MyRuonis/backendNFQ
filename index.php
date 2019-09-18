08add5bfa74b38bdc4d2c30df1945c717d1c6716
MyRuonis <kciulcinskas@gmail.com>
2019-09-18 16:22:57 +0300

    Working buttons

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 <h2>Welcome !!!</h2>
 
-<button type="button" href="administravimas.php">Eilės administravimas</button> 
-<button type="button" href="svieslente.php">Švieslentė</button> 
-<button type="button" href="specialistas.php">Specialistas</button> 
+<a href="administravimas.php">Eilės administravimas</a> 
+<a href="svieslente.php">Švieslentė</a> 
+<a href="specialistas.php">Specialistas</a> 
 
 </body>
