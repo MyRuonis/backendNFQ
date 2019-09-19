e01da4344f7f9a8823b783e187c8e86cf7789818
MyRuonis <kciulcinskas@gmail.com>
2019-09-19 20:56:54 +0300

    bugFix

--- specialistas.php
+++ specialistas.php
@@ -34,8 +34,8 @@ try {
                     <?php foreach ($stocks as $stock) : ?>
                         <tr>
                             <form action="/delPat.php" method="post">
-                                <td><textarea name='name'><?php echo htmlspecialchars($stock['name']); ?></textarea></td>
-                                <td><textarea name='time'><?php echo htmlspecialchars($stock['time']); ?></textarea></td>
+                                <td><a name='name'><?php echo htmlspecialchars($stock['name']); ?></a></td>
+                                <td><a name='time'><?php echo htmlspecialchars($stock['time']); ?></a></td>
                                 <td><input type="submit" value="Aptarnautas"></td>
                             </form>
                         </tr>
