d14b860ee59d8c21eb2f98841f81caf6ef054aad
MyRuonis <kciulcinskas@gmail.com>
2019-09-19 18:49:02 +0300

    none

--- sukurtiKlienta.php
+++ sukurtiKlienta.php
@@ -3,12 +3,12 @@
 require 'vendor/autoload.php';
  
 use namesql\Connection as Connection;
+use namesql\Sqlhphpinsert as Sqlhphpinsert;
 
- 
 try {
     $vard = $_GET['vardas'];
     $pdo = Connection::get()->connect();
-    
+
     //echo 'A connection to the PostgreSQL database sever has been established successfully.';
 
     $insertDemo = new Sqlhphpinsert($pdo);
