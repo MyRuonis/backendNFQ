7fd51561a863f9fd03ab927ec1571ee3022bc54f
MyRuonis <kciulcinskas@gmail.com>
2019-09-20 21:50:46 +0300

    navbar

--- index.php
+++ index.php
@@ -29,10 +29,4 @@
     </div>
 </nav>
 
-<h2>Welcome !!!</h2>
-
-<a href="administravimas.php">Eilės administravimas</a> 
-<a href="svieslente.php">Švieslentė</a> 
-<a href="specialistas.php">Specialistas</a> 
-
 </body>
--- specialistas.php
+++ specialistas.php
@@ -14,36 +14,60 @@ try {
     echo $e->getMessage();
 }
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
-    </head>
-    <body>
-        <div class="container">
-            <h1>Klientai</h1>
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Time Registered</th>
-                        <th>Specialistas</th>
-                    </tr>
+
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">SVEIKI!</a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+  </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="administravimas.php">Registracija<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="svieslente.php">Švieslentė<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="specialistas.php">Specialistas<span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="container">
+    <h1>Klientai</h1>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Name</th>
+                    <th>Time Registered</th>
+                   <th>Specialistas</th>
+                </tr>
                 </thead>
                 <tbody>
-                    <?php foreach ($stocks as $stock) : ?>
-                        <tr>
-                            <form action="/delPat.php" method="get">
-                                <?php $value1=htmlspecialchars($stock['name']); $value2=htmlspecialchars($stock['regtime']); $value3=htmlspecialchars($stock['specialistas']);?>
-                                <td><input type="hidden" name="name" value="<?php echo $value1; ?>"><?php echo $value1; ?><br></td>
-                                <td><input type="hidden" name="regtime" value="<?php echo $value2; ?>"><?php echo $value2; ?><br></td>
-                                <td><input type="hidden" name="specialistas" value="<?php echo $value3; ?>"><?php echo $value3; ?><br></td>
-                                <td><input type="submit" value="Aptarnautas"></td>
-                            </form>
-                        </tr>
-                    <?php endforeach; ?>
+                <?php foreach ($stocks as $stock) : ?>
+                    <tr>
+                    <form action="/delPat.php" method="get">
+                        <?php $value1=htmlspecialchars($stock['name']); $value2=htmlspecialchars($stock['regtime']); $value3=htmlspecialchars($stock['specialistas']);?>
+                        <td><input type="hidden" name="name" value="<?php echo $value1; ?>"><?php echo $value1; ?><br></td>
+                        <td><input type="hidden" name="regtime" value="<?php echo $value2; ?>"><?php echo $value2; ?><br></td>
+                        <td><input type="hidden" name="specialistas" value="<?php echo $value3; ?>"><?php echo $value3; ?><br></td>
+                        <td><input type="submit" value="Aptarnautas"></td>
+                    </form>
+                    </tr>
+                <?php endforeach; ?>
                 </tbody>
-            </table>
-        </div>
-    </body>
-</html>
+    </table>
+</div>
+</body>
