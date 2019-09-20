4a0989176e41f3f8eb277ab8cbcfb5e9d02be3cc
MyRuonis <kciulcinskas@gmail.com>
2019-09-20 22:45:53 +0300

    bugfix

--- app/dbpatient.php
+++ app/dbpatient.php
@@ -84,7 +84,12 @@ class dbpatient
                 . 'WHERE name = "' . $specialistas . '";');
         $time = date("H:i:s");
         while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
-            $time = $row['bendrassugaistaslaikas'] / $row['aptarnautiklientai'];
+            $str_time = $row['bendrassugaistaslaikas'];
+            $str_time = preg_replace("/^([\d]{1,2})\:([\d]{2})$/", "00:$1:$2", $str_time);
+            sscanf($str_time, "%d:%d:%d", $hours, $minutes, $seconds);
+            $time_seconds = $hours * 3600 + $minutes * 60 + $seconds;
+
+            $time = $time_seconds / $row['aptarnautiklientai'];
         }
 
         $stmt = $this->pdo->query('SELECT id '
@@ -96,7 +101,7 @@ class dbpatient
             $klientuKiekis += 1;
         }
 
-        return $time / $klientuKiekis;
+        return date("H:i:s", $time * $klientuKiekis);
     }
 }
 
