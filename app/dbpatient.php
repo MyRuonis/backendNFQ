01039f203a5935a71a9c15bfb22854a2600dbec3
MyRuonis <kciulcinskas@gmail.com>
2019-09-21 22:27:21 +0300

    bugFix

--- app/dbpatient.php
+++ app/dbpatient.php
@@ -166,12 +166,11 @@ class dbpatient
 
         $duomenys1 = $duomenys2 = 0;
 
-        $stmt = $this->pdo->query('SELECT * '
+        $stmt = $this->pdo->query('SELECT name, regtime, endtime, aptarnautas, specialistas '
                 . 'FROM patients '
                 . 'WHERE id = :id;');
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(':id', $helpid1);
-        $stmt->execute();
         while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
             $duomenys1 = array($row['name'],$row['regtime'],$row['endtime'],$row['aptarnautas'],$row['specialistas']);
         }
