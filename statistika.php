83d6aafacb7b086fc974f3b057a3ca3d429498c7
MyRuonis <kciulcinskas@gmail.com>
2019-09-22 13:25:25 +0300

    StatisticsTable

--- statistika.php
+++ statistika.php
@@ -1,20 +1,3 @@
-<?php
-require 'vendor/autoload.php';
- 
-use namesql\Connection as Connection;
-use namesql\dbstats as dbstats;
- 
-try {
-    $pdo = Connection::get()->connect();
-
-    $stockDB = new dbstats($pdo);
-
-    $stocks = $stockDB->laisva("Dantistas");
-} catch (\PDOException $e) {
-    echo $e->getMessage();
-}
-?>
-
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -48,5 +31,35 @@ try {
         </ul>
     </div>
 </nav>
+
+<table class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">Laikas Nuo - Iki</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>
+        <?php
+        require 'vendor/autoload.php';
+        
+        use namesql\Connection as Connection;
+        use namesql\dbstats as dbstats;
+        
+        try {
+            $pdo = Connection::get()->connect();
+
+            $stockDB = new dbstats($pdo);
+
+            $stocks = $stockDB->laisva("Dantistas");
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+        }
+        ?>
+      </td>
+    </tr>
+  </tbody>
+</table>
 
 </body>
