6a93a3140fe2d7bde3f6452d2ba4b236edd27ddf
MyRuonis <kciulcinskas@gmail.com>
2019-09-22 10:56:57 +0300

    bugFix

--- app/dbpatient.php
+++ app/dbpatient.php
@@ -167,7 +167,7 @@ class dbpatient
     }
 
     public function pavelinti($name, $regTime, $specialistas){
-        $sql = 'SELECT id, name, regtime FROM patients WHERE CAST(regtime as Time) >= Cast(:regtime, as Time) AND specialistas = :specialistas AND aptarnautas = false;';
+        $sql = 'SELECT id, name, regtime FROM patients WHERE CAST(regtime as Time) >= Cast(:regtime as Time) AND specialistas = :specialistas AND aptarnautas = false;';
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(':regtime', $regTime);
         $stmt->bindValue(':specialistas', $specialistas);
